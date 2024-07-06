ec80a61e4eecb5f1e9a41cb5c9dc01650cd632c3
Adam Mckaig <adam.mckaig@gmail.com>
2024-07-06 15:38:16 -0400

    add e (exit) and r (reset) keys

--- sandbox/main.py
+++ sandbox/main.py
@@ -6,30 +6,47 @@ import mujoco.viewer
 import arena
 import controllers
 
-a = arena.Arena()
-m, d = a.get()
+# signals
+reset = False
+exit = False
 
-ctrl = controllers.Cheater(a.camera().id, a.target().id)
+def keypress(code: int):
+    global reset, exit
+    print(f"keypress: code={code}")
 
-with mujoco.viewer.launch_passive(m, d) as viewer:
-  while viewer.is_running():
-    start = time.time()
-    mujoco.mj_step(m, d)
+    if code == 69: # e: exit
+        exit = True
+    elif code == 82: # r: reset
+        reset = True
 
-    # apply changes to the viewer.
-    viewer.sync()
+while True:
+   
+    aren = arena.Arena()
+    m, d = aren.get()
+    ctrl = controllers.Cheater(aren.camera().id, aren.target().id)
 
-    # if done, then stop.
-    if ctrl.done(d):
-       print("Done!")
-       break
+    with mujoco.viewer.launch_passive(m, d, key_callback=keypress) as viewer:
+        while viewer.is_running():
+            start = time.time()
+            mujoco.mj_step(m, d)
 
-    # otherwise, apply the next action.
-    # TODO: for the actual model, this should take pixels, not the actual data!
-    ctrl.act(d)
+            # if done, then stop.
+            if reset or exit or ctrl.done(d):
+                reset = False
+                break
 
-    # wait until its time to advance.
-    # this is pasted from the examples and is NOT accurate.
-    time_until_next_step = m.opt.timestep - (time.time() - start)
-    if time_until_next_step > 0:
-      time.sleep(time_until_next_step)
+            # apply changes to the viewer.
+            viewer.sync()
+
+            # otherwise, apply the next action.
+            # TODO: for the actual model, this should take pixels, not the actual data!
+            ctrl.act(d)
+
+            # wait until its time to advance.
+            # this is pasted from the examples and is NOT accurate.
+            time_until_next_step = m.opt.timestep - (time.time() - start)
+            if time_until_next_step > 0:
+                time.sleep(time_until_next_step)
+
+    if exit:
+        break
