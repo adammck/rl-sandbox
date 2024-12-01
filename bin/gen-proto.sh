85fa673ee771e9784d20717224bfe6fe6c73af69
Adam Mckaig <adam.mckaig@gmail.com>
2024-12-01 02:29:41 -0500

    add very weird proto hack
    i don't really care enough about this to figure out how to fix it
    properly. i think i'm using protos wrong for python. it seems like they
    really want to be on the pythonpath? i am probably just confused

--- bin/gen-proto.sh
+++ bin/gen-proto.sh
@@ -4,4 +4,9 @@ cd "$(dirname "$0")"
 
 pushd ../proto
 python -m grpc_tools.protoc -I. --python_out=gen --grpc_python_out=gen ./*.proto
+
+# fix imports in the generated grpc file
+# i have no idea why this isn't an arg to protoc
+# https://github.com/protocolbuffers/protobuf/issues/1491
+sed -i '' 's/^import collector_pb2/import proto.gen.collector_pb2/' gen/collector_pb2_grpc.py
 popd
